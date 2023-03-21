493f980220b3030a45115e01b25e53b0c7ace6b6
Krushna1621 <121724011+Krushna1621@users.noreply.github.com>
2023-03-21 13:26:57 +0530

    document_file_added

---
+++ document.js
@@ -0,0 +1,12 @@
+//document.getElementsByClassName('list-group-item');
+//console.log(document.all[10]);
+//document.all[10].textContent='Hello';
+//console.log(document.forms);
+//console.log(document.getElementsByClassName('header-title'));
+//var a=document.getElementById('header-title');
+//a.innerHTML="krushnakant shinde";
+//a.style.color="red";
+//a.innerText="Bhagvat Shinde";
+//getelementbyid has the structure like document.getElementById("idname").that proprties and then = value;
+document.getElementById('header-title').innerHTML="<h1>Krushnakant shinde</h1>"
+document.getElementById('header-title').style.background="black";
